f3d04a119dbcf10d714aa7717a6471297fe5f78b
R10725045 <r10725045@ntu.edu.tw>
2022-05-28 01:44:05 +0800

    add bigtable functions

--- ticket-api/app.py
+++ ticket-api/app.py
@@ -1,43 +1,44 @@
-from flask import Flask, jsonify, request
-from google.cloud import bigtable
-from google.cloud.bigtable import column_family
-from google.cloud.bigtable import row_filters
+from flask import Flask, jsonify, request, Response
 import bigtable
+import utils
 
 
 app = Flask(__name__)
+
 
 @app.route('/ticket_api/', methods=['GET'])
 def test_api():
     # bigtable.create_table("tictake")
     return jsonify(message='Hello, API')
 
-@app.route('/ticket_api/', methods=['POST'])
-def add_ticket_order():
-    try:
-        data = request.get_json()
-        activity = Activity(
-            activity_name = data['activity_name'], 
-            start_date = utc8_to_gmt(data['start_date']),
-            end_date = utc8_to_gmt(data['end_date']),
-            total_inventory = data['total_inventory'],
-            remaining_inventory = data['remaining_inventory'],
-            activity_info = data['activity_info'],
-            user_id = data['user_id']
-        )
-        db.session.add(activity)
-        db.session.commit()
-        activity_id = activity.activity_id
-        resp = jsonify(
-            activity_id = activity.activity_id
-        )
 
-        return resp
-    except Exception as e:
-        print(e)
-        return Response(status=500)
-    return Response(status=500)
-    # return jsonify(message='Hello, API')
+@app.route('/add_ticket_order/', methods=['POST'])
+def add_ticket_order():
+    data = request.get_json()
+    member = data['member']
+    activity_id = data['activity_id']
+    has_paid = data['has_paid']
+    order_timestamp = utils.utc8_to_gmt(data['order_timestamp'])
+    bigtable.create_order(member, activity_id, has_paid, order_timestamp)
+
+    resp = jsonify(success="true")
+    return resp
+
+    # try:
+    #     data = request.get_json()
+    #     member = data['member']
+    #     activity_id = data['activity_id']
+    #     has_paid = data['has_paid']
+    #     order_timestamp = utils.utc8_to_gmt(data['order_timestamp'])
+    #     bigtable.create_order(member, activity_id, has_paid, order_timestamp)
+
+    #     resp = jsonify(success="true")
+    #     return resp
+    # except Exception as e:
+    #     print(e)
+    #     return Response(status=500)
+    # return Response(status=500)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ticket-api/bigtable.py
+++ ticket-api/bigtable.py
@@ -1,17 +1,18 @@
 import datetime
+from flask import jsonify
 from google.cloud import bigtable
 from google.cloud.bigtable import column_family
 from google.cloud.bigtable import row_filters
 from google.oauth2 import service_account
 
+# https://cloud.google.com/bigtable/docs/samples-python-hello
+# https://googleapis.dev/python/bigtable/latest/index.html
 
 credentials = service_account.Credentials. from_service_account_file(
     'tictake-303073cdcbf0.json')
 client = bigtable.Client(project="tictake", admin=True,
                          credentials=credentials)
 instance = client.instance("instance-tictake")
-
-# bigtable
 
 
 def create_table(table_id):
@@ -36,20 +37,24 @@ def create_order(
         order_timestamp,
         table_id="tictake",
         column_family_id="activity"):
+
     table = instance.table(table_id)
     # print("Writing some greetings to the table.")
     # greetings = ["Hello World!", "Hello Cloud Bigtable!", "Hello Python!"]
     # rows = []
     # for i, value in enumerate(greetings):
     row_key = "{}#{}#{}".format(member, activity_id, order_timestamp).encode()
+    print(row_key)
     row = table.direct_row(row_key)
-    dt = datetime.datetime.now()
+    utc_datetime = datetime.datetime.utcnow()
+    row.set_cell(column_family_id, "activity_id".encode(), activity_id,
+                 timestamp=utc_datetime)
     row.set_cell(column_family_id, "member".encode(), member,
-                 timestamp=datetime.datetime.utcnow())
+                 timestamp=utc_datetime)
     row.set_cell(column_family_id, "has_paid".encode(),
-                 has_paid, timestamp=datetime.datetime.utcnow())
+                 has_paid, timestamp=utc_datetime)
     row.set_cell(column_family_id, "order_timestamp".encode(),
-                 order_timestamp, timestamp=datetime.datetime.utcnow())
+                 str(order_timestamp).encode(), timestamp=utc_datetime)
     # rows.append(row)
     table.mutate_rows(row)
     # table.mutate_rows(rows)
@@ -72,4 +77,17 @@ def get_order(
         order_timestamp,
         table_id="tictake",
         column_family_id="activity"):
-    pass
+
+    table = instance.table(table_id)
+    print("Getting a single greeting by row key.")
+    key = "{}#{}#{}".format(member, activity_id, order_timestamp).encode()
+    row_filter = row_filters.CellsColumnLimitFilter(1)
+    row = table.read_row(key, row_filter)
+    member_cell = row.cells[column_family_id]["member".encode()][0]
+    activity_id_cell = row.cells[column_family_id]["activity_id".encode()][0]
+    has_paid_cell = row.cells[column_family_id]["has_paid".encode()][0]
+    order_timestamp_cell = row.cells[column_family_id]["order_timestamp".encode()][0]
+    return jsonify(member=member_cell.value.decode("utf-8"),
+                   activity_id=activity_id_cell.value.decode("utf-8"),
+                   has_paid=has_paid_cell.value.decode("utf-8"),
+                   order_timestamp=order_timestamp_cell.value.decode("utf-8"))
