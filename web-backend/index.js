8c55f109d65a7e12f4e943ad898bf76a50c762f0
disam8853 <wang.sam33@gmail.com>
2022-06-06 14:22:47 +0800

    imp login and signup

--- web-backend/index.js
+++ web-backend/index.js
@@ -24,21 +24,35 @@ if ([USER_ACTIVITY_API, TICKET_API, JWT_KEY].some((k) => !k)) {
 app.post('/api/signup', async (req, res) => {
   const userInfo = req.body
   try {
-    const signupRes = await axios.post(`${USER_ACTIVITY_API}/users`, userInfo)
+    const signupRes = await axios.post(`${USER_ACTIVITY_API}/api/v1/user/create`, userInfo)
     res.send(signupRes.body)
-  } catch (err) {
-    res.status(400).send('signup failed!')
+  } catch (error) {
+    if (error.response) {
+      return res.status(error.response.status).send('error\n' + error.response.data)
+    }
+    return res.status(500).send('error\n' + error)
   }
 })
 
-app.post('/api/login', (req, res) => {
+app.post('/api/login', async (req, res) => {
+  try {
+    const body = {
+      email: req.body.email,
+      password: req.body.password,
+    }
+    await axios.post(`${USER_ACTIVITY_API}/api/v1/user/login`, body)
+  } catch (error) {
+    if (error.response) {
+      return res.status(error.response.status).send('error\n' + error.response.data)
+    }
+    return res.status(500).send('error\n' + error)
+  }
+
   const user = {
-    first_name: 'Sam',
-    last_name: 'Wang',
-    email: 'abc12345@ntu.edu.tw',
+    email: req.body.email,
   }
   // expires in one hour
-  const token = jwt.sign({ user }, JWT_KEY, { expiresIn: 60 * 60 })
+  const token = jwt.sign({ user }, JWT_KEY, { expiresIn: process.env.JWT_EXP_TIME || 60 * 60 })
   res.cookie('access_token', token)
   res.send('Login success!')
 })
